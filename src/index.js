9781fa821d8293a0fcf5a1a7b0ac1d8f6ea60eb1
Mahammad Aslam <middeaslam36@gmail.com>
2022-10-13 16:06:41 +0530

    'Elements'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+const element = React.createElement('h1', null, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {element}
   </React.StrictMode>
 );
 
@@ -15,3 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
