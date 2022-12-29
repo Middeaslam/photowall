35151fbabff7b743b7c91901a137cbdcf4b3244e
Mahammad Aslam <middeaslam36@gmail.com>
2022-12-29 16:34:42 +0530

    Updated for integration

--- src/index.js
+++ src/index.js
@@ -25,15 +25,28 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+
+export const App = () => {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <Main />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
